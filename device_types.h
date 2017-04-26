15090a4ac4b85c597ed20cbd4217142623ae0a0c
James Devine <j.devine@lancaster.ac.uk>
2017-04-26 10:18:48 +0100

    initial commit, moving target to a library

---
+++ device_types.h
@@ -0,0 +1,15 @@
+#ifndef DEVICE_SIZE_H
+#define DEVICE_SIZE_H
+
+#include "stdint.h"
+
+#define PROCESSOR_WORD_TYPE     uint32_t
+
+#include "mbed.h"
+namespace codal
+{
+    typedef PinName PinName;
+    typedef PinMode PinMode;
+}
+
+#endif
