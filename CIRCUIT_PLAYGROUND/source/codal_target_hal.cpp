19b1dc0987a75e255111ae23e68cf0999418bf51
James Devine <j.devine@lancaster.ac.uk>
2017-05-27 17:04:18 +0100

    Added HAL implementation, and updated getMSP for the fiber interface

---
+++ CIRCUIT_PLAYGROUND/source/codal_target_hal.cpp
@@ -0,0 +1,47 @@
+#include "mbed.h"
+#include "codal_target_hal.h"
+
+void target_enable_irq()
+{
+    #warning "COMPILING"
+    __enable_irq();
+}
+
+void target_disable_irq()
+{
+    __disable_irq();
+}
+
+void target_wait_for_event()
+{
+    __WFE();
+}
+
+void target_wait(uint32_t milliseconds)
+{
+    wait_ms(milliseconds);
+}
+
+void target_reset()
+{
+    NVIC_SystemReset();
+}
+
+void target_panic(int statusCode)
+{
+    target_disable_irq();
+
+#if DEVICE_DMESG_BUFFER_SIZE > 0
+    DMESG("*** CODAL PANIC : [%d]", statusCode);
+    while (1)
+    {
+    }
+#else
+    Serial pc(USBTX, USBRX);
+    while (1)
+    {
+        pc.printf("*** CODAL PANIC : [%.3d]\n", statusCode);
+        wait_ms(500);
+    }
+#endif
+}
--- device_fiber.h
+++ device_fiber.h
@@ -4,8 +4,6 @@
 #include "device_types.h"
 #include "CodalDevice.h"
 
-extern CodalDevice& device;
-
 /**
   *  Thread Context for an ARM Cortex M0 core.
   *
@@ -71,7 +69,7 @@ inline PROCESSOR_WORD_TYPE tcb_get_stack_base(PROCESSOR_TCB* tcb)
 
 inline PROCESSOR_WORD_TYPE get_current_sp()
 {
-    return device.getMSP();
+    return __get_MSP();
 }
 
 inline PROCESSOR_WORD_TYPE tcb_get_sp(PROCESSOR_TCB* tcb)
